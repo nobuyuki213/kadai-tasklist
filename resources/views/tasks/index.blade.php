6aab85d9066fbb012c143a6ada6cc73e14739dda
EC2 Default User <ec2-user@ip-172-31-42-43.us-east-2.compute.internal>
2018-05-21 03:57:28 +0000

    index task view corection

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,33 +8,33 @@
     
     <p>{!! Html::decode(link_to_route('tasks.create', '<i class="fas fa-plus-circle"></i> Create new task!', array(), ['class' => 'btn btn-primary'])) !!}</p>
     <div>
-      <table class="table">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">No.</th>
-            <th scope="col">Task</th>
-            <th scope="col">Status</th>
-            <th scope="col" class="text-center"><i class="far fa-edit"></i></th>
-            <th scope="col" class="text-center"><i class="far fa-trash-alt"></i></th>
-          </tr>
-        </thead>
-        <tbody>
-          @if (count($tasks) > 0)
-              @foreach($tasks as $task)
-                  <tr>
-                    <th scope="row">{{ $task->id }}</th>
-                    <td>{{ link_to_route('tasks.show', $task->content, ['id' => $task->id]) }}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{!! Html::decode(link_to_route('tasks.edit', '<i class="far fa-edit"></i>', ['id' => $task->id], ['class' => 'btn btn-success'])) !!}</td>
-                    <td>
-                      {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete', 'class' => 'form-group']) !!}
-                        {!! Form::button('<i class="far fa-trash-alt"></i>', ['class' => 'btn btn-dark', 'type' => 'submit']) !!}
-                      {!! Form::close() !!}
-                    </td>
-                  </tr>
-              @endforeach
-          @endif
-        </tbody>
-      </table>
+      @if (count($tasks) > 0)
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Task</th>
+              <th scope="col">Status</th>
+              <th scope="col" class="text-center"><i class="far fa-edit"></i></th>
+              <th scope="col" class="text-center"><i class="far fa-trash-alt"></i></th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($tasks as $task)
+                <tr>
+                  <th scope="row">{{ $task->id }}</th>
+                  <td>{{ link_to_route('tasks.show', $task->content, ['id' => $task->id]) }}</td>
+                  <td>{{ $task->status }}</td>
+                  <td class="text-center">{!! Html::decode(link_to_route('tasks.edit', '<i class="far fa-edit"></i>', ['id' => $task->id], ['class' => 'btn btn-success'])) !!}</td>
+                  <td class="text-center">
+                    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete', 'class' => 'form-group']) !!}
+                      {!! Form::button('<i class="far fa-trash-alt"></i>', ['class' => 'btn btn-dark', 'type' => 'submit']) !!}
+                    {!! Form::close() !!}
+                  </td>
+                </tr>
+            @endforeach
+          </tbody>
+        </table>
+      @endif
     </div>
 @endsection
